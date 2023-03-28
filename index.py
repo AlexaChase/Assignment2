92d01d42856d29ee30edd6571d4fd3a31d02d6f0
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-03-28 23:53:39 +0000

    new files

---
+++ index.py
@@ -0,0 +1,6 @@
+print("Hello,this website is used to calulcate the area and circumference of a circle using the diameter")
+diameter= input ("Please enter the diameter of the circle:")
+import math
+    if diameter == int, float, 
+    
+radius= input(diameter) * 2
