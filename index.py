84ba8806e5f6503b4c54eba0b701b648521d0835
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-03-30 14:51:33 +0000

    new changes

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from math import pi
 diameter=input ("Please enter the diameter of the circle:")
-radius= (diameter/2)
+radius= diameter/2
 if diameter == float:
     area= radius.exp(2)*pi
     print(area)
