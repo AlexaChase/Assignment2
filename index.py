0c5fafcc4e3bab43f951e4d27bfb7d724d336162
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-03-30 13:53:21 +0000

    new changes

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
 print("Hello,this website is used to calulcate the area and circumference of a circle using the diameter")
 diameter= input ("Please enter the diameter of the circle:")
 import math
-    if diameter == int, float, 
-    
-radius= input(diameter) * 2
+    if diameter == isnumeric()
+        continue
+    else:
+        print("Error")
+        break
+    radius= input(diameter) / 2
+    area= radius.exp(2)* (radius.pi/radius)
+    print(area)
