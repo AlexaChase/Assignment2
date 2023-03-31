a889c2a6a9c8ca083e4b7b06b6c7e318d4db6c6c
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-03-31 14:35:51 +0000

    new changes

--- program.py
+++ program.py
@@ -3,3 +3,5 @@ Circlediameter= float(input("Please enter the diameter of the circle:"))
 if Circlediameter>0:
     Circleradius= float(Circlediameter/2)
     print(Circleradius)
+else:
+    print ("Error")
