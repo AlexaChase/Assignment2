f8477b7d6c5c6def34ebec5395d9c8c6d855f24c
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-03-31 14:14:01 +0000

    new changes

--- index.py
+++ index.py
@@ -1,10 +1,5 @@
 from math import pi
-d= input ("Please enter the diameter of the circle:")
-if d == float or int:
-    r= input(d)/2 #division
-    a= r.exp(2)*pi
-    print(a)
-    c = 2 * r * pi
-    print(c)
-else:
-    print("Error")
+Diameter=input("Please enter the diameter of the circle:")
+if Diameter== int() or float():
+    Radius= Diameter/2
+    print(Radius)
