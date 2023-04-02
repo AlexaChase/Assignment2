6d662c90ec488e3980e943ace7c1ed2c2fe18673
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-04-02 17:44:58 +0000

    new changes

--- handler.php
+++ handler.php
@@ -1,4 +1,20 @@
 <!DOCTYPE html>
 <html>
-
-</html>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+        <title>Calculate the area and circumference of a circle using the diameter</title>
+    </head>
+ 
+    <body>
+        <?php
+        $p= 3.141592653589793238462643383279502884197;
+        $CircleDiamater = "";
+        $CircleRadius = $CircleDiamater/2;
+        $CircleArea= $p * $CircleRadius.pow(2);
+        $CircleCircumference= 2 * $p * $CircleRadius;
+        if ($CircleDiamater > "0") {
+            echo "The area of the circle:";
+            echo ($CircleArea);
+            echo "The circumference of the circle:";
+            echo ($CircleCircumference);
+        }
