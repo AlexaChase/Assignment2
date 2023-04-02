19331e677f85133114687f9ef1550a4f5446d983
AlexaChase <124379295+AlexaChase@users.noreply.github.com>
2023-04-02 17:02:54 +0000

    new changes

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ from math import pi
 Circlediameter= float(input("Please enter the diameter of the circle:"))
 if Circlediameter>0:
     Circleradius= float(Circlediameter/2)
-    print(Circleradius)
+    CircleArea= pi * Circleradius**2
+    print(CircleArea)
 elif Circlediameter<=0:
     print("Error, number must be positive")
